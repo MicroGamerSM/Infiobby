8b62d9c10fffdc322e2e19f1bf9c11b95b66117c
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-09-25 09:57:29 -0400

    added GetPlayersInZone util.

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -1,5 +1,26 @@
+const Players = game.GetService("Players");
+
 export function WaitUntil(predicate: () => boolean) {
 	while (!predicate()) {
 		task.wait();
 	}
+}
+
+export function getPlayersInZone(zone: BasePart): Player[] {
+	const playersInside: Player[] = [];
+
+	// Must have CanTouch = true for GetTouchingParts to work
+	zone.CanTouch = true;
+
+	for (const touching of zone.GetTouchingParts()) {
+		const character = touching.Parent;
+		if (!character) continue;
+
+		const player = Players.GetPlayerFromCharacter(character);
+		if (player && !playersInside.includes(player)) {
+			playersInside.push(player);
+		}
+	}
+
+	return playersInside;
 }
