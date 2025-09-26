1d115841197b4feee02405d6120904588c19bf0a
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-09-26 09:53:24 -0400

    [NC] Started the HandleCommand function.

--- src/server/ExtremeAdminCommands/eac.server.ts
+++ src/server/ExtremeAdminCommands/eac.server.ts
@@ -2,7 +2,22 @@ const Players = game.GetService("Players");
 
 const adminIds: number[] = [5742671914];
 
-Players.PlayerAdded.Connect((player: Player) => {
+// Example usage in Roblox-TS
+const result = parseIfPrefixed("!hello world test", "!");
+print(result); // ["hello", "world", "test"]
+
+function HandleCommand(player: Player, command: string, ...args: string[]) {
+	switch (command) {
+		case "/exit":
+			player.Kick("You have exited.");
+			break;
+	}
+}
+
+Players.PlayerAdded.Connect((player) => {
 	if (adminIds.find((value) => value === player.UserId) === undefined) return;
 	print("hi admin");
+	player.Chatted.Connect((message) => {
+		HandleCommand(player, ...message.split(" "));
+	});
 });
