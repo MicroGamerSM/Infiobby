fed806f0e18e7040df3d1b77e5ed340e48be83a7
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-09-26 05:24:18 -0400

    RoundState now keeps track of its machine.

--- src/server/RoundManager/RoundState.ts
+++ src/server/RoundManager/RoundState.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { IState } from "shared/stateMachine/IState";
+import { StateMachine } from "shared/stateMachine/StateMachine";
 import { Possible } from "shared/types";
 
-export type RoundContext = [];
+export type RoundContext = { machine: StateMachine<RoundContext, RoundSignal> };
 
 export type RoundSignal = [];
 
@@ -17,7 +18,7 @@ export class RoundState implements IState<RoundContext, RoundSignal> {
 		print(`Got signal ${signal}`);
 	}
 
-	Enter(source: Possible<IState<RoundContext, RoundSignal>>, context: []): void {
+	Enter(source: Possible<IState<RoundContext, RoundSignal>>, context: RoundContext): void {
 		print(`Entering from state ${source}`);
 		this.context = context;
 		this.onEnter(source);
