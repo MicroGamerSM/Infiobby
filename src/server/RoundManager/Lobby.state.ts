8bbd091295631efea60eb66c15718e2142403486
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-09-26 08:49:52 -0400

    Removed leftover state machine logic.

--- src/server/RoundManager/Lobby.state.ts
+++
@@ -1,9 +0,0 @@
-import { IState } from "shared/stateMachine/IState";
-import { RoundContext, RoundSignal, RoundState } from "./RoundState";
-import { Possible } from "shared/types";
-
-export class LobbyState extends RoundState {
-	onEnter(source: Possible<IState<RoundContext, RoundSignal>>, context: RoundContext): void {}
-
-	onExit(target: IState<RoundContext, RoundSignal>): void {}
-}
