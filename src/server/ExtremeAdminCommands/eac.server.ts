e55e68e6752839bf072523e41269b9e200672f6e
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-09-30 11:05:21 -0400

    Added kick command.

--- src/server/ExtremeAdminCommands/eac.server.ts
+++ src/server/ExtremeAdminCommands/eac.server.ts
@@ -1,4 +1,4 @@
-import { StartsWith, RemovePrefix } from "shared/util";
+import { StartsWith, RemovePrefix, StringToPlayer } from "shared/util";
 
 const Players = game.GetService("Players");
 
@@ -7,9 +7,19 @@ const prefix: string = "!";
 
 function HandleCommand(player: Player, command: string, ...args: string[]) {
 	switch (command) {
-		case "exit":
+		case "exit": {
 			player.Kick("You have exited.");
 			break;
+		}
+		case "kick": {
+			const target = StringToPlayer(args[0]);
+			const messageParts = args;
+			messageParts.remove(0);
+			let message = messageParts.join(" ");
+			if (message === "" || message === " ") message = "You were kicked by an administrator.";
+			target?.Kick(message);
+			break;
+		}
 	}
 }
 
